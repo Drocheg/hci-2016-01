4acc6d5500658c37914f28f8782cbf0c75521d8d
Juan Li Puma <juanlipuma94@gmail.com>
2016-04-28 22:26:48 -0300

    Added multiple static flight pages to showcase different stages of the flight search process.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,8 @@
 $(function() {  //Document.ready
     //Enable the side navigation bar
     $('.button-collapse').sideNav();
-    //Start the slider
-    $('.slider').slider({
-        full_width: true,
-        height:600});
+    //Initialize select drop-downs, if any
+    $('select').material_select();
     //Initialize RETURN date picker
     $('#return').pickadate({
         min: new Date(),    //Can't travel in the past =(
@@ -16,6 +14,17 @@ $(function() {  //Document.ready
         min: new Date(),    //Can't travel in the past =(
         selectMonths: true, //Creates a dropdown to control month
         selectYears: 2,      //Creates a dropdown of 2 years to control year
+        //Spanish translation https://github.com/amsul/pickadate.js/blob/3.5.6/lib/translations/es_ES.js
+        monthsFull: [ 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre' ],
+        monthsShort: [ 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic' ],
+        weekdaysFull: [ 'domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado' ],
+        weekdaysShort: [ 'dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb' ],
+        today: 'hoy',
+        clear: 'borrar',
+        close: 'cerrar',
+        firstDay: 1,
+        format: 'dddd d !de mmmm !de yyyy',
+        formatSubmit: 'yyyy/mm/dd',
         //When picking a date, close this selector and open the second one with the same date as minimum
         onSet: function() {
             console.log("Chose depart date: "+this.get('select', 'yyyy-mm-dd'));
