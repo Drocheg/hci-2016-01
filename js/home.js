a67b21c159cff8b5e514570161a091d206869248
Juan Li Puma <juanlipuma94@gmail.com>
2016-05-07 15:37:32 -0300

    Added TODO file, updated index file
    -Depart date picker doesn't automatically open the return date picker on
    selection.
    -One-way trip checkbox hides/shows return date picker appropriately when
    clicked.
    -Moved home code from init.js to home.js

---
+++ js/home.js
@@ -0,0 +1,55 @@
+$(function () {
+    var datePickerOptions = {
+        min: new Date(),    //Can't travel in the past =(
+        selectMonths: true,
+        selectYears: 2,     //Creates a dropdown of 2 years ahead to control year
+        //Spanish translation https://github.com/amsul/pickadate.js/blob/3.5.6/lib/translations/es_ES.js
+        monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthsShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+        weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        weekdaysShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+        today: 'hoy',
+        clear: 'borrar',
+        close: 'cerrar',
+        firstDay: 1,
+        format: 'dddd d/m',
+        formatSubmit: 'yyyy-mm-dd',
+        closeOnSelect: true
+    };
+    $('.datepicker').pickadate(datePickerOptions);
+//    $('#returnDate').pickadate(datePickerOptions);
+//    $('#departDate').pickadate(datePickerOptions);
+
+    //Hide/show return date picker when clicking one-way only
+    //http://stackoverflow.com/a/7031408
+    $("#isOneWayTrip").on('change', function () {
+        if ($(this).is(":checked")) {
+            $("#returnDate").fadeOut();
+            $("#returnDate").removeAttr("required");
+            $("label[for=returnDate]").fadeOut();
+        }
+        else {
+            $("#returnDate").fadeIn();
+            $("label[for=returnDate]").fadeIn();
+            $("#returnDate").attr("required", "required");
+        }
+    });
+    
+    //Handle flight search form submit
+    $("#flightSearchForm").on("submit", function(event) {
+        event.preventDefault();
+        var data = {
+            from: $("#from").val(),
+            to: $("#to").val(),
+            departDate: new Date($("input[name=departDate_submit]").val()),
+            isOneWayTrip: $("#isOneWayTrip").is(":checked"),
+            returnDate: new Date($("input[name=returnDate_submit]").val()),
+            numAdults: Number($("#numAdults").val()),
+            numChildren: Number($("#numChildren").val()),
+            numInfants: Number($("#numInfants").val())
+        };
+        //TODO validar info e ir a flights.html con estos parametros en texto, la página rellenará el formulario automáticamente
+        //Y hará la búsqueda por API
+        console.log(data);
+    });
+});
--- js/init.js
+++ js/init.js
@@ -3,42 +3,4 @@ $(function() {  //Document.ready
     $('.button-collapse').sideNav();
     //Initialize select drop-downs, if any
     $('select').material_select();
-    //Initialize RETURN date picker
-    $('#return').pickadate({
-        min: new Date(),    //Can't travel in the past =(
-        selectMonths: true, //Creates a dropdown to control month
-        selectYears: 2      //Creates a dropdown of 2 years to control year
-      });
-    //Initialize DEPART date picker
-    $('#depart').pickadate({
-        min: new Date(),    //Can't travel in the past =(
-        selectMonths: true, //Creates a dropdown to control month
-        selectYears: 2,      //Creates a dropdown of 2 years to control year
-        //Spanish translation https://github.com/amsul/pickadate.js/blob/3.5.6/lib/translations/es_ES.js
-        monthsFull: [ 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre' ],
-        monthsShort: [ 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic' ],
-        weekdaysFull: [ 'domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado' ],
-        weekdaysShort: [ 'dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb' ],
-        today: 'hoy',
-        clear: 'borrar',
-        close: 'cerrar',
-        firstDay: 1,
-        format: 'dddd d !de mmmm !de yyyy',
-        formatSubmit: 'yyyy/mm/dd',
-        //When picking a date, close this selector and open the second one with the same date as minimum
-        onSet: function() {
-            console.log("Chose depart date: "+this.get('select', 'yyyy-mm-dd'));
-            this.close();   //Close current selector
-            //TODO hacer que el selector de regreso se abra automaticamente con fecha minima seleccionada
-//            //Re-initialize the return date picker with the new parameters
-//            $('#return').pickadate({
-//                min: new Date(this.get('select', 'yyyy-mm-dd')),
-//                selectMonths: true, //Creates a dropdown to control month
-//                selectYears: 2      //Creates a dropdown of 2 years to control year
-//              });
-            
-//            returnDate.set({min: this.get('value')}); //Set min date for the return trip
-//            returnDate.open();    //Open the return selector
-        }
-      });
 });
