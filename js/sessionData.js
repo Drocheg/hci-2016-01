23eda74da0715795788c968d6d57cc8c484d2a53
Juan Li Puma <juanlipuma94@gmail.com>
2016-05-10 17:53:59 -0300

    Added js/sessionData.js to get an idea of how to store session data accross pages.

---
+++ js/sessionData.js
@@ -0,0 +1,27 @@
+//TO GET THE OBJECT
+var session = JSON.parse(sessionStorage.sessionData);   //will return UNDEFINED if it doesn't exist yet
+//TO SAVE IT AGAIN (make sure you got it first)
+sessionStorage.sessionData = JSON.stringify(session);
+
+var sessionData = {
+    search: {
+        from: "airport or city ID",
+        to: "airport or cityID",
+        isOneWayTrip: false,
+        depart: new Date(),         //date includes date and time
+        return: new Date() || null, //null in case of one-way trip
+        //what else?
+    },
+    flights: {
+        outboundFlights: [new Flight(), new Flight()],          //more than 1 flight if there are stopovers
+        inboundFlights: [new Flight(), new Fligh()] || null,    //can be null if one-way trip, otherwise same as outboundFlights
+        total: 42,                                              //Can be calculated by adding flights' cost, provided for convenience
+        //what else?
+    },
+    preferences: {
+        currency: "currencyCode",       //Default to USD? Or current location?
+        currencyExchangeRate: 42,       //Calculated with API using currency
+        language: "en",                 //In case we support multi-language site (probably won't)
+        //what else?
+    }
+};
