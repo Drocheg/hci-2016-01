1fe75781aaa530f5b8c0f71f37d4a1f4976ebd99
Facugf <facundogonzalezf@gmail.com>
2016-05-11 16:01:11 -0300

    Falta validar variables, chequear si esta todo bien creado. Warning: CopyPasta incoming

--- js/passengers.js
+++ js/passengers.js
@@ -3,5 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+$(function() {  //Es alto copypasta de payment
+    $("#passengers-form").on("submit", function(event) {
+        event.preventDefault();
+        var data = {
+            nombre: $("#nombre").val(),
+            apellido: $("#apellido").val(),
+            sexo: $("#sexo").val(),
+            documento: $("#documento").val(),
+            dia: $("#dia").val(),
+            mes: $("#mes").val(),
+            anio: $("#anio").val()
+        };
+        //Validar
+        
+        var session = JSON.parse(sessionStorage.sessionData);   //will return UNDEFINED if it doesn't exist yet
+        session.payment = data;
+        sessionStorage.sessionData = JSON.stringify(session);
+    });
+});
