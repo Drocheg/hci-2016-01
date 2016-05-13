d158b3b79c8a83412ba79aa7c8b127c71503038c
Droche <ddegrimaudetderocheb@itba.edu.ar>
2016-05-13 12:52:44 -0300

    andan los select

--- js/controllers.js
+++ js/controllers.js
@@ -229,12 +229,4 @@ app.controller('controller', function ($scope, $http, $log) {
         return $scope.colors;
     };
     
-     $scope.colors = [
-      {name:'black', shade:'dark'},
-      {name:'white', shade:'light', notAnOption: true},
-      {name:'red', shade:'dark'},
-      {name:'blue', shade:'dark', notAnOption: true},
-      {name:'yellow', shade:'light', notAnOption: false}
-    ];
-    $scope.myColor = $scope.colors[2]; // red
 });
