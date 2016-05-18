657272ed600ab6cfe8d5059ba03b17bb0fe3b78c
fgonzalez <fgonzalez@localhost.localdomain>
2016-05-18 15:02:25 -0300

    Primera implementacion de pasajeros en summary

--- js/order-summary.js
+++ js/order-summary.js
@@ -1,102 +1,64 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function getOriginAirport(flight) {
+    return flight.outbound_routes[0].segments[0].departure.airport;
+}
+
+function getDepartureDateObj(flight) {
+    return new Date(flight.outbound_routes[0].segments[0].departure.date);
+}
+
+function getDestinationAirport(flight) {
+    return flight.outbound_routes[0].segments[0].arrival.airport;
+}
+
+function getArrivalDateObj(flight) {
+    return new Date(flight.outbound_routes[0].segments[0].arrival.date);
+}
+
+function getStopovers(flight) {
+    return flight.outbound_routes[0].segments[0].stopovers;
+}
+
+function getStopoversCount(flight) {
+    return flight.outbound_routes[0].segments[0].stopovers.length;
+}
+
+function isDirectFlight(flight) {
+    return getStopovers(flight).length === 0;
+}
+
+function getFlightTotal(flight) {
+    return getFlightPriceBreakdown(flight).total.total;
+}
+
+function getFlightPriceBreakdown(flight) {
+    return flight.price;
+}
+
+function getFlightAirlineName(flight) {
+    return flight.outbound_routes[0].segments[0].airline.name;
+}
+
+function getFlightAirlineID(flight) {
+    return flight.outbound_routes[0].segments[0].airline.id;
+}
+
+function getFlightAirlineLogoURL(flight) {
+    var session = getSessionData();
+    return session.airlines[getFlightAirlineID(flight)].logo || "#";
+//    TODO fall back to default image if not found
+}
+
+function getFlightNumber(flight) {
+    return flight.outbound_routes[0].segments[0].number;
+}
+
+function getFlightDuration(flight) {
+    return flight.outbound_routes[0].duration;
+}
+
+
 $(function () {
-//                var session = getSessionData();
-//                var html ="<h3>IDA</h3>";
-//                for(var i=0; i<=getStopoversCount(session.outboundFlight);i++){ //Supongo que el 3 son las escalas
-//                    html += "<h5>Vuelo numero "+i+1+"</h5> \
-//                            <p>Numero de vuelo: "+getFlightNumber(session.outboundFlight)+"</p>\n\
-//                            <p>Fecha: "+(getDepartureDateObj(session.outboundFlight)).getDate()+"/"+getDepartureDateObj(session.outboundFlight).getMonth()+"</p>\n\
-//                            <p>Horario de salida: "+(getDepartureDateObj(session.outboundFlight)).getHours()+":"+getDepartureDateObj(session.outboundFlight).getMinutes()+"</p>\n\
-//                            <P>Horario de llegada: "+(getArrivalDateObj(session.outboundFlight)).getHours()+":"+getArrivalDateObj(session.outboundFlight).getMinutes()+"</p>\n\
-//                            <p>Duracion: "+getFlightDuration(session.outboundFlight)+"</p>\n\ ";  //Falta precio y cantidad de pasajeros
-//                                                    //lo pongo despues que necesita codigo
-//                    if(session.search.numAdults > 0){
-//                    html += "<p> Precio por adultos: $"+getFlightPriceBreakdown(session.outboundFlight).adults.base_fare+"x"+session.search.numAdults+"</p>"; 
-//                } 
-//                 if(session.search.numChildren > 0){
-//                    html += "<p> Precio por ninio: $"+getFlightPriceBreakdown(session.outboundFlight).children.base_fare+"x"+session.search.numChildren+"</p>"; 
-//                } 
-//                  if(session.search.numInfants > 0){
-//                    html += "<p> Precio por infantes: $"+getFlightPriceBreakdown(session.outboundFlight).infants.base_fare+"x"+session.search.numInfants+"</p>"; 
-//                } 
-//                    html += "<p>Subtotal: "+getFlightPriceBreakdown(session.outboundFlight).total.fare+"</p>\n\
-//                            <p>Cargos e impuestos: "+(getFlightPriceBreakdown(session.outboundFlight).total.charges+getFlightPriceBreakdown(session.outboundFlight).total.taxes)+"</p>\n\
-//                            ";
-//    }
-//    html += "<h5><b>Total:</b></h5>";
-//    $("#outbound").html(html);
-//    
-//    if(!session.search.OneWayTrip)
-//    html = "<h3>VUELTA</h3>";
-//    for(var i=0; i<=getStopoversCount(session.outboundFlight);i++){ //Supongo que el 3 son las escalas
-//                    html += "<h5>Vuelo numero "+i+1+"</h5> \
-//                            <p>Numero de vuelo: "+getFlightNumber(session.outboundFlight)+"</p>\n\
-//                            <p>Fecha: "+(getDepartureDateObj(session.outboundFlight)).getDate()+"/"+getDepartureDateObj(session.outboundFlight).getMonth()+"</p>\n\
-//                            <p>Horario de salida: "+(getDepartureDateObj(session.outboundFlight)).getHours()+":"+getDepartureDateObj(session.outboundFlight).getMinutes()+"</p>\n\
-//                            <P>Horario de llegada: "+(getArrivalDateObj(session.outboundFlight)).getHours()+":"+getArrivalDateObj(session.outboundFlight).getMinutes()+"</p>\n\
-//                            <p>Duracion: "+getFlightDuration(session.outboundFlight)+"</p>\n\ ";  //Falta precio y cantidad de pasajeros
-//                                                    //lo pongo despues que necesita codigo
-//                    if(session.search.numAdults > 0){
-//                    html += "<p> Precio por adultos: $"+getFlightPriceBreakdown(session.outboundFlight).adults.base_fare+"x"+session.search.numAdults+"</p>"; 
-//                } 
-//                 if(session.search.numChildren > 0){
-//                    html += "<p> Precio por ninio: $"+getFlightPriceBreakdown(session.outboundFlight).children.base_fare+"x"+session.search.numChildren+"</p>"; 
-//                } 
-//                  if(session.search.numInfants > 0){
-//                    html += "<p> Precio por infantes: $"+getFlightPriceBreakdown(session.outboundFlight).infants.base_fare+"x"+session.search.numInfants+"</p>"; 
-//                } 
-//                    html += "<p>Subtotal: "+getFlightPriceBreakdown(session.outboundFlight).total.fare+"</p>\n\
-//                            <p>Cargos e impuestos: "+(getFlightPriceBreakdown(session.outboundFlight).total.charges+getFlightPriceBreakdown(session.outboundFlight).total.taxes)+"</p>\n\
-//                            ";
-//    }                          //lo pongo despues que necesita codigo
-//    html += "<h5><b>Total:</b></h5>";
-//    $("#inbound").html(html);
-//
-//
-//    $("#payment").html("\
-//                            <h4>Información de pago</h4> \
-//                            <p>Número de tarjeta: " + session.payment.cardNumber + "</p> \
-//                            <p>Nombre de titular: " + session.payment.cardholderName + "</p> \
-//                            <br> \
-//                            <h5><b>Gran total: $27.998</b></h5> \
-//                   ");
-//    $("#contact").html("\
-//                            <h4>Información de contacto</h4> \
-//                            <p>Email: " + session.payment.email + "</p> \
-//                    ");
-//                $("#inbound").html("\
-//                        <h4>Vuelta</h4> \
-//                        <p>Vuelo: Delta #101</p> \
-//                        <p>Fecha: " + session.search.departDate +"</p> \
-//                        <p>Horario de salida: 09:15</p> \
-//                        <p>Horario de llegada: 16:30(-1)</p> \
-//                        <p>Escalas: 3</p> \
-//                        <p>Duración: 4:30</p> \
-//                        <p>Precio por adulto: $10.000</p> \
-//                        <p>Cantidad de pasajeros: " +session.search.numAdults+" adultos; <br> "+session.search.childern+" ninios <br> "+session.search.numInfants+" infantes</p> \
-//                        <p>Subtotal: $10.000</p> \
-//                        <p>Cargos e impuestos: $3.999</p> \
-//                        <br> \
-//                        <p><b>Total: $13.999</b></p> \
-//                        ");
-//                $("#outbound").html("<h4>IDA</h4> \
-//                                <p>Vuelo: Delta #110</p> \
-//                                <p>Fecha: " + session.search.returnDate + "</p> \
-//                                <p>Horario de salida: 21:15</p> \
-//                                <p>Horario de llegada: 5:30(+1) 27/12/2016</p> \
-//                                <p>Escalas: 3</p> \
-//                                <p>Duración: 4:30</p> \
-//                                <p>Precio por adulto: $20.000</p> \
-//                                <p>Cantidad de pasajeros: " +session.search.numAdults+" adultos; <br> "+session.search.childern+" ninios <br> "+session.search.numInfants+" infantes</p> \
-//                                <p>Subtotal: $10.000</p> \
-//                                <p>Cargos e impuestos: $3.999</p> \
-//                                <br> \
-//                                <p><b>Total: $13.999</b></p>\
-//                    ");
+
 
     $("#changeOutboundFlightBtn").on("click", function () {
         var session = getSessionData();
